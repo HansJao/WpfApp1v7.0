0125a8aa840abe937094689d79e835e9d259e8b2
Hanhan <vigorhan@gmail.com>
2019-01-31 17:39:22 +0800

    故障處理
    頁面初步配置與部份資料

--- WpfApp1/Pages/FabricPages/NewMalFunctionPage.xaml.cs
+++ WpfApp1/Pages/FabricPages/NewMalFunctionPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.DataClass.Entity;
 using WpfApp1.ViewModel.FabricViewModel;
 
 namespace WpfApp1.Pages.FabricPages
@@ -25,6 +26,52 @@ namespace WpfApp1.Pages.FabricPages
         {
             InitializeComponent();
             this.DataContext = new NewMalFunctionViewModel();
+        }
+
+        private void CustomerList_KeyUp(object sender, KeyEventArgs e)
+        {
+            CollectionView itemsViewOriginal = (CollectionView)CollectionViewSource.GetDefaultView(CustomerList.ItemsSource);
+
+            itemsViewOriginal.Filter = ((o) =>
+            {
+                if (String.IsNullOrEmpty(CustomerList.Text)) return true;
+                else
+                {
+                    if ((((Customer)o).Name).Contains(CustomerList.Text)) return true;
+                    else return false;
+                }
+            });
+
+            itemsViewOriginal.Refresh();
+
+            // if datasource is a DataView, then apply RowFilter as below and replace above logic with below one
+            /* 
+             DataView view = (DataView) Cmb.ItemsSource; 
+             view.RowFilter = ("Name like '*" + Cmb.Text + "*'"); 
+            */
+        }
+
+        private void FabricList_KeyUp(object sender, KeyEventArgs e)
+        {
+            CollectionView itemsViewOriginal = (CollectionView)CollectionViewSource.GetDefaultView(FabricList.ItemsSource);
+
+            itemsViewOriginal.Filter = ((o) =>
+            {
+                if (String.IsNullOrEmpty(FabricList.Text)) return true;
+                else
+                {
+                    if ((((Fabric)o).FabricName).Contains(FabricList.Text)) return true;
+                    else return false;
+                }
+            });
+
+            itemsViewOriginal.Refresh();
+
+            // if datasource is a DataView, then apply RowFilter as below and replace above logic with below one
+            /* 
+             DataView view = (DataView) Cmb.ItemsSource; 
+             view.RowFilter = ("Name like '*" + Cmb.Text + "*'"); 
+            */
         }
     }
 }
--- WpfApp1/ViewModel/FabricViewModel/NewMalFunctionViewModel.cs
+++ WpfApp1/ViewModel/FabricViewModel/NewMalFunctionViewModel.cs
@@ -19,82 +19,33 @@ namespace WpfApp1.ViewModel.FabricViewModel
         protected ICustomerModule CustomerModule { get; } = new CustomerModule();
 
 
-        private string _customerName { get; set; }
-        public string CustomerName
+        private Customer _customer { get; set; }
+        public Customer Customer
         {
-            get { return _customerName; }
-            set
-            {
-                _customerName = value;
-                string filterText = value;
-                ICollectionView cv = CollectionViewSource.GetDefaultView(CustomerNameList);
-                if (!string.IsNullOrEmpty(filterText))
-                {
-                    cv.Filter = o =>
-                    {
-                        /* change to get data row value */
-                        string p = o as string;
-                        return (p.ToUpper().Contains(filterText.ToUpper()));
-                        /* end change to get data row value */
-                    };
-                }
-                else
-                {
-                    cv.Filter = o =>
-                    {
-                        return (true);
-                    };
-                }
-            }
+            get { return _customer; }
+            set { _customer = value; }
         }
 
-        public IEnumerable<string> CustomerNameList { get; set; }
-        private string _fabricName { get; set; }
-        public string FabricName
-        {
-            get { return _fabricName; }
-            set
-            {
-                _fabricName = value;
-                string filterText = value;
-                ICollectionView cv = CollectionViewSource.GetDefaultView(FabricList);
-                if (!string.IsNullOrEmpty(filterText))
-                {
-                    cv.Filter = o =>
-                    {
-                        /* change to get data row value */
-                        Fabric p = o as Fabric;
-                        return (p.FabricName.ToUpper().Contains(filterText.ToUpper()));
-                        /* end change to get data row value */
-                    };
-                }
-                else
-                {
-                    cv.Filter = o =>
-                    {
-                        return (true);
-                    };
-                }
-            }
-        }
-        public IEnumerable<Fabric> FabricList { get; set; }
+        public IEnumerable<Customer> CustomerList { get; set; }
 
-        private Fabric _selectedFabric { get; set; }
-        public Fabric SelectedFabric
+        private Fabric _fabric { get; set; }
+        public Fabric Fabric
         {
-            get { return _selectedFabric; }
+            get { return _fabric; }
             set
             {
-                _selectedFabric = value;
+                _fabric = value;
                 FabricColorList = FabricModule.GetFabricColorListByFabricID(new List<int> { value.FabricID });
                 RaisePropertyChanged("FabricColorList");
             }
         }
+        public IEnumerable<Fabric> FabricList { get; set; }      
 
         public IEnumerable<FabricColor> FabricColorList { get; set; }
+        public FabricColor FabricColor { get; set; }
         public NewMalFunctionViewModel()
         {
-            CustomerNameList = CustomerModule.GetCustomerNameList();
+            CustomerList = CustomerModule.GetCustomerList();
             FabricList = FabricModule.GetFabricList();
         }
     }
