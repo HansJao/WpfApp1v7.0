ccee6d7c977988d17377059b8a246e0d6758b80c
Hanhan <vigorhan@gmail.com>
2019-05-06 14:40:19 +0800

    介面優化

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -48,32 +48,38 @@ namespace WpfApp1
 
         private void ButtonStoreSearchFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "庫存查詢";
             this.MainFrame.NavigationService.Navigate(new StoreSearchPage());
 
         }
 
         private void ButtonShippingFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "出貨";
             this.MainFrame.NavigationService.Navigate(new ShippingPage());
         }
 
         private void ButtonReportsFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "報表"; 
             this.MainFrame.NavigationService.Navigate(new ReportsPage());
         }
 
         private void ButtonReportsByCustomerFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "帳務系統表參考";
             this.MainFrame.NavigationService.Navigate(new TrashSystemTablePage());
         }
 
         private void ButtonProcessOrderRecordFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "訂單紀錄";
             this.MainFrame.NavigationService.Navigate(new ProcessOrderPage());
         }
 
         private void ButtonNewProcessOrderRecordFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增訂單";
             this.MainFrame.NavigationService.Navigate(new NewProcessOrderPage());
         }
 
@@ -85,6 +91,7 @@ namespace WpfApp1
 
         private void ButtonFactoryManageFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增工廠";
             this.MainFrame.NavigationService.Navigate(new AddFactory());
         }
 
@@ -96,41 +103,50 @@ namespace WpfApp1
 
         private void ButtonFactoryList_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增工廠";
             this.MainFrame.NavigationService.Navigate(new FactoryListPage());
         }
 
         private void ButtonCustomerAddFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增客戶";
             this.MainFrame.NavigationService.Navigate(new AddCustomerPage());
         }
         private void ButtonCustomerList_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "客戶清單";
             this.MainFrame.NavigationService.Navigate(new CustomerListPage());
         }
 
         private void ButtonFabricManageFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增布種";
             this.MainFrame.NavigationService.Navigate(new NewFabricPage());
         }
         private void ButtonFeatureSearchFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "特徵搜尋";
             this.MainFrame.NavigationService.Navigate(new FeatureSearchPage());
         }
         private void ButtonFabricCostQueryFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "布種成本查詢";
             this.MainFrame.NavigationService.Navigate(new FabricCostQueryPage());
         }
 
         private void ButtonYarnPriceFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "紗價管理";
             this.MainFrame.NavigationService.Navigate(new YarnPricePage());
         }
         private void ButtonNewMalFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "新增故障處理";
             this.MainFrame.NavigationService.Navigate(new MalFunctionPage());
         }
         private void ButtonInventoryRecordCompareFunction_Click(object sender, RoutedEventArgs e)
         {
+            this.Title = "庫存歷史紀錄";
             this.MainFrame.NavigationService.Navigate(new InventoryRecordComparePage());
         }
     }
